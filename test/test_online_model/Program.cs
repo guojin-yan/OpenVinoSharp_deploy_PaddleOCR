1d730cc653a5cd4d6c0fe9f48751bcd6308855e7
yanguojin <guojin_yjs@cumt.edu.cn>
2024-04-29 19:02:15 +0800

    Add test_online_model project.

--- test/test_online_model/Program.cs
+++ test/test_online_model/Program.cs
@@ -1,43 +1,17 @@
-﻿using System.IO;
-using System;
-using PaddleOCR;
-using iTextSharp.xmp.impl.xpath;
-using System.IO.Compression;
-using iTextSharp.xmp.impl;
-using SharpCompress.Readers.Tar;
-using OpenVinoSharp.Extensions.Utility;
-
+﻿using OpenCvSharp;
+using OpenVinoSharp.Extensions.model.PaddleOCR;
 
 namespace test_online_model
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            string url = "https://paddleocr.bj.bcebos.com/PP-OCRv4/chinese/ch_PP-OCRv4_rec_infer.rar";
-            string path = "./../../../../model/";
-            string file_path = Path.Combine(path, Path.GetFileName(url));
-            //_ = Utility.download_file_async(url, file_path).Result;
-            //ZipFile.ExtractToDirectory(file_path, path);
-
-            Utility.unzip(file_path, path);
-            Console.WriteLine("Hello, World!");
-
-            //ConsoleUtility.WriteProgressBar(0);
-            //for (var i = 0; i <= 100; ++i)
-            //{
-            //    ConsoleUtility.WriteProgressBar(i, true);
-            //    Thread.Sleep(50);
-            //}
-            //Console.WriteLine();
-            //ConsoleUtility.WriteProgress(0);
-            //for (var i = 0; i <= 100; ++i)
-            //{
-            //    ConsoleUtility.WriteProgress(i, true);
-            //    Thread.Sleep(50);
-            //}
-
+            //OcrModel ocrModel = await OcrModel.GetOnlineOcrModel(Language.ch_PP_OCRv3,false,false,true);
+            OnlineOcr ocr = await Pipeline.GetOnlineOCR(Language.ch_PP_OCRv4);
+            List<OCRPredictResult> ocr_result = ocr.predict();
+            PaddleOcrUtility.print_result(ocr_result);
+            //Mat new_image = PaddleOcrUtility.visualize_bboxes(image, ocr_result);
         }
     }
 }
