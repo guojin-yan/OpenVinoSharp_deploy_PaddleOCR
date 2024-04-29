1120aa5a338e14c5dc9cc2a597756b2f3939ff7d
yanguojin <guojin_yjs@cumt.edu.cn>
2024-04-29 19:00:50 +0800

    Add OcrConsoleNet6.0 project.

---
+++ sample/OcrConsoleNet3.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using OpenVinoSharp.Extensions.model.PaddleOCR;
+namespace OcrConsoleNet3._1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            OnlineOcr ocr = await Pipeline.GetOnlineOCR(Language.ch_PP_OCRv4);
+            Tuple<List<OCRPredictResult>, Mat> ocr_result = ocr.ocr_test();
+            PaddleOcrUtility.print_result(ocr_result.Item1);
+        }
+    }
+}
